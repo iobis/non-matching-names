01231caf25f5f512ca9ea5498012a7a719d91764
Pieter Provoost <pieterprovoost@gmail.com>
2021-07-11 22:38:24 +0200

    using new API endpoint for VLIZ

--- script.py
+++ script.py
@@ -2,19 +2,14 @@ import requests
 import csv
 import pandas as pa
 import math
-res = requests.get("https://api.obis.org/checklist/nomatch").json()["results"]
 
-names = [{
-    "scientificName": name["scientificName"],
-    "records": name["records"],
-    "scientificNameID": "|".join([id["scientificNameID"] for id in name["scientificNameID"]])
-} for name in res]
+names = requests.get("https://api.obis.org/checklist/nomatchvliz").json()["results"]
 
 vliz_list = pa.read_excel("vliz_list/Blacklist_update_20200626.xlsx", engine="openpyxl", na_values=["NA"], converters={"annotation_type": str, "annotation_resolved_AphiaID": str})
 vliz_list.set_index("scientificname_original", inplace=True)
 
 for i in range(len(names)):
-    name = names[i]["scientificName"]
+    name = names[i]["scientificname"]
     if name in vliz_list.index:
         entries = vliz_list.loc[[name]]
         if len(entries) == 1:
@@ -22,7 +17,7 @@ for i in range(len(names)):
             if isinstance(entries["annotation_resolved_AphiaID"][0], str) or not math.isnan(entries["annotation_resolved_AphiaID"][0]):
                 names[i]["annotation_aphiaid"] = entries["annotation_resolved_AphiaID"][0]
 
-csv_cols = ["scientificName", "records", "scientificNameID", "annotation_type", "annotation_aphiaid"]
+csv_cols = ["scientificname", "scientificnameid", "records", "phylum", "class", "order", "family", "genus", "annotation_type", "annotation_aphiaid", "datasets"]
 
 with open("list_annotated.csv", "w") as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=csv_cols)
